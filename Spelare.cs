318b6b17d075eab7185d26b185b01d9205de099d
bithor <8bitgubben@gmail.com>
2020-07-10 02:06:16 +0200

    varsågod henrik

--- Spelare.cs
+++ Spelare.cs
@@ -10,6 +10,11 @@ namespace CocaineCrackDown {
         public SpriteBatch SpriteBatch;
         public float GolvTopSlut { get; set; }
 
+        public enum riktning {
+            höger,
+            vänster
+        }
+
     }
 
 
@@ -26,23 +31,26 @@ namespace CocaineCrackDown {
 
     public class Spelare : Hjälte {
         //////////////////////////////////////////
-        public float SkalaPåSpelarna = 2.0f;
+        public float SkalaPåSpelarna = 1.0f;
         //////////////////////////////////////////
         public float Höjd { get; set; }
         public float Bredd { get; set; }
         public float SkärmBredd { get; set; }
         public float SkärmHöjd { get; set; }
         public bool AttackStatus { get; set; }
+        public riktning SpelarRiktning { get; set; }
         public void GåVänster(float RörelseHastighet) {
             X -= RörelseHastighet;
             if (X < 1) {
                 X = 1;
+                SpelarRiktning = riktning.vänster;
             }
         }
         public void GåHöger(float RörelseHastighet) {
             X += RörelseHastighet;
             if ((X + Bredd) > SkärmBredd) {
                 X = SkärmBredd - Bredd;
+                SpelarRiktning = riktning.höger;
             }
         }
         public void GåUpp(float RörelseHastighet) {
@@ -76,7 +84,7 @@ namespace CocaineCrackDown {
             SpriteBatch = spritebatch;
             SkärmBredd = skärmbredd;
             SkärmHöjd = skärmhöjd;
-            //                halva skärm            spelar textur höjd gånger skala delat på 8 gånger 7  
+            //                halva skärm            spelar textur höjd gånger skala delat på 8 gånger 7
             GolvTopSlut = (skärmhöjd / 2) - (SpelareEttTextur.Height * SkalaPåSpelarna / 8 * 7);
 
         }
@@ -84,14 +92,23 @@ namespace CocaineCrackDown {
             AttackStatus = _AttackStatus;
         }
         public void Draw() {  //bild      position       rectangel   färg  rotation  origin      skala   SpriteEffect    layerdeapth
-            if (AttackStatus == true) {
-                SpriteBatch.Draw(SpelareEttAttackTextur, new Vector2(X, Y), null, Color.DodgerBlue, 0, new Vector2(0, 0), SkalaPåSpelarna, SpriteEffects.None, 0);
-            }
-            else {
-                SpriteBatch.Draw(SpelareEttTextur, new Vector2(X, Y), null, Color.DodgerBlue, 0, new Vector2(0, 0), SkalaPåSpelarna, SpriteEffects.None, 0);
+            if(SpelarRiktning == riktning.höger) {
+                if (AttackStatus == true) {
+                    SpriteBatch.Draw(SpelareEttAttackTextur, new Vector2(X, Y), null, Color.DodgerBlue, 0, new Vector2(0, 0), SkalaPåSpelarna, SpriteEffects.None, 0);
+                }
+                else {
+                    SpriteBatch.Draw(SpelareEttTextur, new Vector2(X, Y), null, Color.DodgerBlue, 0, new Vector2(0, 0), SkalaPåSpelarna, SpriteEffects.None, 0);
+                }
+            }else if(SpelarRiktning == riktning.vänster) {
+                if (AttackStatus == true) {
+                    SpriteBatch.Draw(SpelareEttAttackTextur, new Vector2(X, Y), null, Color.DodgerBlue, 0, new Vector2(0, 0), SkalaPåSpelarna, SpriteEffects.FlipHorizontally, 0);
+                }
+                else {
+                    SpriteBatch.Draw(SpelareEttTextur, new Vector2(X, Y), null, Color.DodgerBlue, 0, new Vector2(0, 0), SkalaPåSpelarna, SpriteEffects.FlipHorizontally, 0);
+                }
             }
         }
-        
+
     }
     public class SpelareTvå : Spelare {
         private Texture2D SpelareTvåTextur { get; set; }
@@ -110,7 +127,7 @@ namespace CocaineCrackDown {
             SpriteBatch = spritebatch;
             SkärmBredd = skärmbredd;
             SkärmHöjd = skärmhöjd;
-            //                halva skärm            spelar textur höjd gånger skala delat på 8 gånger 7  
+            //                halva skärm            spelar textur höjd gånger skala delat på 8 gånger 7
             GolvTopSlut = (skärmhöjd / 2) - (SpelareTvåTextur.Height * SkalaPåSpelarna / 8 * 7);
 
         }
