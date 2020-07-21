c52881c2fb57333193c136f9f12730b31e98a281
Anders H. Andersson <35273566+WeanlinBoof@users.noreply.github.com>
2020-07-21 02:38:40 +0200

    Bruh

---
+++ CCD/CCD/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CCD {
+    public static class Program {
+        [STAThread]
+        static void Main() {
+            using Spel CCD = new Spel();
+            CCD.Run();
+        }
+    }
+}
