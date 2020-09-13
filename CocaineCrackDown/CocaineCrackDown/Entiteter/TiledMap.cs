9adc2f1e20dac3cdb891c2192403548c7e66f109
Anders H. Andersson <35273566+WeanlinBoof@users.noreply.github.com>
2020-09-13 13:10:53 +0200

    tile entitet

---
+++ CocaineCrackDown/CocaineCrackDown/Entiteter/TiledMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Nez;
+
+namespace CocaineCrackDown.Entiteter {
+    public class TiledMap : Entity {
+        public override void OnAddedToScene() {
+            base.OnAddedToScene();
+        }
+
+        public override void OnRemovedFromScene() {
+            base.OnRemovedFromScene();
+        }
+
+        public override void Update() {
+            base.Update();
+        }
+    }
+}
