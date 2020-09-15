7020c1d4df100ef907bd52ded9bf258d3c7f16a0
bithor <8bitgubben@gmail.com>
2020-09-15 23:02:52 +0200

    :((

--- CocaineCrackDown/CocaineCrackDown/Entiteter/Spelare.cs
+++ CocaineCrackDown/CocaineCrackDown/Entiteter/Spelare.cs
@@ -18,19 +18,25 @@ namespace CocaineCrackDown.Entiteter {
         public double SenasteUpdateringsTid { get; set; }
         public Scene Scen { get; set; }
         protected InmatningsHanterare inmatningsHanterare;
+        protected RörelseKomponent rörelseKomponent;
+        protected AtlasAnimationKomponent atlasAnimationsKomponent; 
+        protected FollowCamera followCamera; 
         public Spelare(string namn) {
             Namn = namn;
+            inmatningsHanterare = new InmatningsHanterare();
+            rörelseKomponent = new RörelseKomponent(inmatningsHanterare , RörelseHastighet);
+            atlasAnimationsKomponent = new AtlasAnimationKomponent(inmatningsHanterare);
+            followCamera = new FollowCamera(this);
+            Core.RegisterGlobalManager(inmatningsHanterare);
         }
         public bool Lokal { get; set; }
         public override void OnAddedToScene() {
             Name = Namn;
             Position = new Vector2(Scene.SceneRenderTargetSize.X/2, Scene.SceneRenderTargetSize.Y / 2);
-            inmatningsHanterare = new InmatningsHanterare();
-            Core.RegisterGlobalManager(inmatningsHanterare);
             //AddComponent(new KollisionsKomponent());
-            AddComponent(new RörelseKomponent(inmatningsHanterare , RörelseHastighet));
-            AddComponent(new AtlasAnimationKomponent(inmatningsHanterare));
-            AddComponent(new FollowCamera(this));
+            AddComponent(rörelseKomponent);
+            AddComponent(atlasAnimationsKomponent);
+            AddComponent(followCamera);
         }
 
         public override void OnRemovedFromScene() {
@@ -47,10 +53,10 @@ namespace CocaineCrackDown.Entiteter {
             Lokal = lokal;
         }
     }
+
     public class Randy : Spelare {
         public Randy(string namn = "randy", bool lokal = false) : base(namn) {
             Lokal = lokal;
-
         }
     }
 }
